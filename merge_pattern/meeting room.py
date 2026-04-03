e3cc6c4a1b8077f161353713e31587a0b97a0148
Vaishali-Sonkar <vaishalisonker345@gmail.com>
2026-04-03 14:52:20 +0530

    meeting room

--- merge_pattern/meeting room.py
+++ merge_pattern/meeting room.py
@@ -2,19 +2,19 @@
 
 class Solution:
     def minMeetingRooms(self, start, end):
-        start.sort()
-        end.sort()
+        start.sort()        #sort the start and end times of the meetings
+        end.sort() 
         n=len(start)
-        room=0
+        room=0       
         res=0
         i=0
         j=0
         while(i<n and j<n):
-            if(start[i]<end[j]):
+            if(start[i]<end[j]):     #start is small then firstly room will be allocated
                 room+=1
                 res=max(res,room)
-                i+=1
-            else:
-                room-=1
+                i+=1                   
+            else:                  #if end is small then room will be deallocated
+                room-=1             
                 j+=1
         return res
