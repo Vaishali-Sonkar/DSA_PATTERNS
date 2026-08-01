5a6af89c221b18edf22ad57c1dfdfcfa6895e4e5
Vaishali-Sonkar <vaishalisonker345@gmail.com>
2026-08-02 00:02:38 +0530

    added

---
+++ TREE/111.minDepthBT.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minDepth(self, root):
+        if root==None:
+            return 0
+        if root.left==None and root.right==None:
+            return 1
+        if root.left==None:
+            return 1+ self.minDepth(root.right)
+        if root.right==None:
+            return 1+ self.minDepth(root.left)
+        return 1+ min(self.minDepth(root.left),self.minDepth(root.right))
